75efcc038bcff53dd31c60bfb1530b2959e53150
jap1e09 <j.a.pritchard@soton.ac.uk>
2018-07-20 16:08:50 +0100

    Function definition cleared up.
    'main' function definition not required in a function like this.

--- analytics.py
+++ analytics.py
@@ -22,17 +22,16 @@ def indexDataFrame(df, indexColumns, retainCols=False):
         df.set_index(indexColumns, drop=True, inplace=True)
         
 def create_Dfs(filepath):
-    if __name__ == '__main__':
-        indexes = pd.read_hdf(filepath, 'indexes')
-        journeyDf = pd.read_hdf(filepath, 'journeyDf')
-        indexDataFrame(journeyDf, indexes.tolist(), retainCols=True)
-        vehicleDf = pd.read_hdf(filepath, 'vehicleDf')
-        indexDataFrame(vehicleDf, indexes.tolist(), retainCols=False)
-        try:
-            trainDf = pd.read_hdf(filepath, 'trainDf')
-            indexDataFrame(trainDf, indexes.tolist(), retainCols=False)
-        except:
-            pass
+    indexes = pd.read_hdf(filepath, 'indexes')
+    journeyDf = pd.read_hdf(filepath, 'journeyDf')
+    indexDataFrame(journeyDf, indexes.tolist(), retainCols=True)
+    vehicleDf = pd.read_hdf(filepath, 'vehicleDf')
+    indexDataFrame(vehicleDf, indexes.tolist(), retainCols=False)
+    try:
+        trainDf = pd.read_hdf(filepath, 'trainDf')
+        indexDataFrame(trainDf, indexes.tolist(), retainCols=False)
+    except:
+        pass
     journeyDf.insert(4, 'date', journeyDf.UniqueJourneyId.apply(f))
     journeyDf['date'] = pd.to_datetime(journeyDf['date'])
     trainjournDf = pd.concat([journeyDf,trainDf], axis=1, sort='false')
